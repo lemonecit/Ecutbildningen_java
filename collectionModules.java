8064abcc56e7e0c302235c63316166c8efe1df0a
plemonev <pile2100@student.miun.se>
2024-11-01 20:32:42 +0100

    inlammingsuppgift - ecutbildningen

--- collectionModules.java
+++ collectionModules.java
@@ -155,13 +155,13 @@ public class collectionModules {
         System.out.println("Totala bokstäver som har skrivits blir " + getTotalChars());
         System.out.println("Totala rader genom loop blir antal rad " + getTotalLines());
         System.out.println("Det längsta ordet av alla du skrev är \n" + getLongestString() + "\noch antal bokstav blir " + getLongestString().length());
-        
+
         sayBye();
         Tack();
 
 //        Scanner closes completely
         inputing.close();
-//rklekqwlekqlek
+
 
     }
 
