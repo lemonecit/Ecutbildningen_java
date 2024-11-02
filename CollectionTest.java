323ac8a51545c3a6e3157b82f19035ec6ac274e7
plemonev <pile2100@student.miun.se>
2024-11-02 09:56:59 +0100

    inlammingsuppgift - ecutbildningen

--- CollectionTest.java
+++ CollectionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CollectionTest {
-
+//Test and pass if first and second row are counted with 10 chars
     @Test
     void testGetTotalChars() {
         collectionModules check = new collectionModules();
@@ -13,6 +13,10 @@ public class CollectionTest {
         check.countCharAndLine("fghij");
         assertEquals(10, check.getTotalChars());
     }
+
+
+    //Test and pass if rows will be counted in 3
+
 
     @Test
     void testGetTotalLines() {
@@ -23,7 +27,7 @@ public class CollectionTest {
 
         assertEquals(3, check.getTotalLines());
     }
-
+//Test and pass if this will match correct with our test value Worldasdadasdasdasd! is any of these rows with same as test value and pass with green sign
     @Test
     void testGetLongestString() {
         collectionModules check = new collectionModules();
